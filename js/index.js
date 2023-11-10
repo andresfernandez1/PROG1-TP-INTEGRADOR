7c03c426f740e054b4364fcb31a196192b83faa6
andresfernandez1 <andresfernandez@udesa.edu.ar>
2023-11-10 16:21:25 -0300

    index js hecho

---
+++ js/index.js
@@ -0,0 +1,60 @@
+let acaVaLaAPIKey = "e085a8d4a0502afc1d3c8e65c53af130";
+
+let url = `https://api.themoviedb.org/3/movie/top_rated?api_key=${acaVaLaAPIKey}`
+let url2 = `https://api.themoviedb.org/3/tv/popular?api_key=${acaVaLaAPIKey}`
+
+
+let cardsec = document.querySelector("#cardsec");
+let cardter = document.querySelector("#cardter");
+
+fetch(url)
+.then(function(res) {
+    return res.json();
+})
+.then(function(data) {
+
+    let miData = data.results;
+    let contenido = "";
+    for (let i = 0; i < 6; i++) {
+        
+        console.log(miData[i]);
+
+        contenido += `<article class="card">
+                                        <a href="./pelicula.html?id=${miData[i].id}"><img class="img_main" src="https://image.tmdb.org/t/p/w500/${miData[i].poster_path} " alt=""></a>
+                                        <a class="texto-a" href="./pelicula.html">${miData[i].title}</a>
+                                    </article>`
+    }
+
+    cardsec.innerHTML=contenido;
+
+
+})
+.catch(function (err) {
+    console.log(err);
+})
+
+fetch(url2)
+.then(function(res) {
+    return res.json();
+})
+.then(function(data) {
+
+    let miData = data.results;
+    let contenido = "";
+    for (let i = 0; i < 6; i++) {
+        
+        console.log(miData[i]);
+
+        contenido += `<article class="card">
+                                        <a href="./series.html?id=${miData[i].id}"><img class="img_main" src="https://image.tmdb.org/t/p/w500/${miData[i].poster_path} " alt=""></a>
+                                        <a class="texto-a" href="./series.html">${miData[i].name}</a>
+                                    </article>`
+    }
+
+    cardter.innerHTML=contenido;
+
+
+})
+.catch(function (err) {
+    console.log(err);
+})
